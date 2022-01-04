81d94bddbc4e2edbaf2831bd233ba76923d801e5
隔壁的泰山 <3207754367@qq.com>
2022-01-05 05:17:44 +0800

    modified:   app/agconnect-services.json 	modified:   app/build.gradle 	deleted:    app/release/1641324024613.jpg 	deleted:    app/release/1641324024623.jpg 	deleted:    app/release/1641324156292.jpg 	modified:   app/release/app-release.apk 	modified:   app/src/main/java/com/ma/powersoundswitch/activity/SettingActivity.java

--- app/src/main/java/com/ma/powersoundswitch/activity/SettingActivity.java
+++ app/src/main/java/com/ma/powersoundswitch/activity/SettingActivity.java
@@ -28,10 +28,11 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        AGConnectInstance.initialize(this);
-        AppCenter.start(getApplication(), "b5f71581-37c7-42a2-b631-45a8a56a17df", Analytics.class, Crashes.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+
+        AppCenter.start(getApplication(), "b5f71581-37c7-42a2-b631-45a8a56a17df", Analytics.class, Crashes.class);
+
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
